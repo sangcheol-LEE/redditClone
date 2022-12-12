aded067f3582a09169e9e1ae6a697d2aa1b1e4c1
이상철 <zlrz6706@gmail.com>
2022-12-12 21:55:27 +0900

    entity 생성 및 baseEntity생성

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,9 +6,15 @@ const app = express();
 // json 형식으로 요청오는 걸 해석해줌
 app.use(express.json());
 app.use(morgan("dev"));
+import { AppDataSource } from "./data-source"
+
 
 app.get("/",(_, res) => res.send("running"));
 let port = 4000;
 app.listen(port, async () => {
   console.log(`server running at http://localhost:${port}`)
+
+  AppDataSource.initialize().then(() => {
+    console.log("database initialized")
+  }).catch(error => console.log(error))
 })
