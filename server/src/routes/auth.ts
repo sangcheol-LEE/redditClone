e29d6b7e138c886476f3d5ba862d73e66e08095e
이상철 <zlrz6706@gmail.com>
2022-12-15 13:14:34 +0900

    로그인 화면 및  기능 구현 완료

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,14 @@
-import { validate } from "class-validator";
+import { isEmail, isEmpty, validate } from "class-validator";
 import { Request, Router, Response } from "express";
 import { User } from "../entities/User";
-
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import cookie from "cookie";
 
 const mapError = (errors : Object[]) => {
   return errors.reduce((prev: any, err:any) => {
-    prev[err.property] = Object.entries(err.constraints[0][1])
-    return prev
+    prev[err.property] = Object.entries(err.constraints)[0][1]
+    return prev;
   },{})
 }
 
@@ -49,9 +51,51 @@ const register = async(req: Request, res: Response) => {
   }
 }
 
+const login = async(req: Request, res: Response) => {
+  const {email, password} = req.body
+
+  try {
+    let errors: any = {}
+    // 비워져있다면, 프론트엔드로 보내주기
+    if(isEmpty(email)) errors.email = "이메일은 비워둘 수 없습니다."
+    if(isEmpty(password)) errors.password = "비밀번호는 비워둘 수 없습니다."
+    if(Object.keys(errors).length > 0 ){
+      return res.status(400).json({errors})
+    }
+
+    // 디비에서 유저찾기
+    const user = await User.findOneBy({email})
+
+    if(!user) return res.status(404).json({email: "등록된 이메일 주소가 없습니다."})
+
+    // 유저가 있다면 비밀번호 비교하기
+    const passwordMatches = await bcrypt.compare(password, user.password)
+
+    // 비밀번호가 다르다면 에러 보내기
+    if(!passwordMatches) {
+      return res.status(401).json({password : "비밀번호가 잘못되었습니다."});
+    }
+    // 비밀번호가 맞다면 토큰생성
+    const token = jwt.sign({email}, process.env.JWT_SECRET)
+
+    // 쿠키저장
+    res.set("Ian-Cookie", cookie.serialize("token", token,
+      {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7,
+        path: "/",
+      }));
+
+    return res.json({user, token})
+  }catch(error) {
+    console.error(error)
+    return res.status(500).json(error)
+  }
+}
 const router = Router();
 
 router.post("/register", register)
+router.post("/login", login)
 
 
 export default router
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,21 +3,23 @@ import morgan from "morgan"
 import { AppDataSource } from "./data-source"
 import authRoutes from "./routes/auth"
 import cors from "cors"
-
+import dotenv from "dotenv";
 const app = express();
 const origin = "http://localhost:3000";
 app.use(cors({
-  origin
+  origin,
+  credentials: true
 }))
 // json 형식으로 요청오는 걸 해석해줌
 app.use(express.json());
 app.use(morgan("dev"));
 
+dotenv.config()
+
 app.get("/",(_, res) => res.send("running"));
 app.use("/api/auth", authRoutes)
-let port = 4000;
-app.listen(port, async () => {
-  console.log(`server running at http://localhost:${port}`)
+app.listen(process.env.PORT, async () => {
+  console.log(`server running at http://localhost:${process.env.PORT}`)
 
   AppDataSource.initialize().then(() => {
     console.log("database initialized")
